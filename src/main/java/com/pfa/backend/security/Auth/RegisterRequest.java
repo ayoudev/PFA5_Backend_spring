0b6c591ca1d1e5846b49b4d9494b79ee713df9f5
ayoub <hakmaoui@gmail.com>
2024-12-25 23:14:16 +0100

    commit

---
+++ src/main/java/com/pfa/backend/security/Auth/RegisterRequest.java
@@ -0,0 +1,41 @@
+package com.pfa.backend.security.Auth;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    public RegisterRequest(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public RegisterRequest() {
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
