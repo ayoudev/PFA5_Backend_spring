fce5036e61d651750e89adbf0ccb33d032dff2a9
Ayoub Hakmaoui <118122958+ayoudev@users.noreply.github.com>
2024-12-25 23:49:34 +0100

    spring security

--- src/main/java/com/pfa/backend/security/Auth/EmailAlreadyExistsException.java
+++ src/main/java/com/pfa/backend/security/Auth/EmailAlreadyExistsException.java
@@ -4,6 +4,7 @@ package com.pfa.backend.security.Auth;
 
 public class EmailAlreadyExistsException extends RuntimeException {
     public EmailAlreadyExistsException(String message) {
+        
         super(message);
     }
 }
