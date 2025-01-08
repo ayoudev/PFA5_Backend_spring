d4ac81eda382a02384e95234a1a3901c36af8896
Ayoub Hakmaoui <118122958+ayoudev@users.noreply.github.com>
2025-01-08 21:05:53 +0100

    Change_apis.java

--- src/main/java/com/pfa/backend/configuration/GlobalCorsConfig.java
+++ src/main/java/com/pfa/backend/configuration/GlobalCorsConfig.java
@@ -16,7 +16,6 @@ public class GlobalCorsConfig {
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
 
-        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", config);
 
